13e2feca3ee17dc70f24d0ff84bacac2e693db35
JakubStranianek <j.stranianek@gmail.com>
2022-05-20 08:07:19 +0200

    ready for making movie list

--- entertaiment-web-app/src/components/mainApp/mainApp.js
+++ entertaiment-web-app/src/components/mainApp/mainApp.js
@@ -2,17 +2,29 @@ import React from 'react'
 import "./mainApp.scss"
 import ContentBox from "./contentBox.js"
 import searchBox from "../../assets/icon-search.svg"
+import data from "./data.json"
 
 export default function mainApp() {
+  fetch()
+     .then(res=>{
+        this.setState({package:[...this.state.package,res.json()]}) 
+        //using es6
+     })
+     .catch(err=>{
+        console.log(err)
+     })
+
   return (
     <div className='mainApp'>
-      <ContentBox></ContentBox> 
-
+      <ContentBox></ContentBox>
       <div className='searchBox'>
         <img src={searchBox} alt="searchIcon"></img>
         <input type="text" placeholder='Search for movies or TV series'></input>     
       </div>
       
+      <div className='trending'>
+        
+      </div>
     </div>
   )
 }
