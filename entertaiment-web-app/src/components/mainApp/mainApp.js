71664666faae42e1f5ca56434f8d88cc035f3778
JakubStranianek <j.stranianek@gmail.com>
2022-05-22 13:46:21 +0200

    mainPage design done

--- entertaiment-web-app/src/components/mainApp/mainApp.js
+++ entertaiment-web-app/src/components/mainApp/mainApp.js
@@ -1,30 +1,25 @@
-import React from 'react'
-import "./mainApp.scss"
-import ContentBox from "./contentBox.js"
-import searchBox from "../../assets/icon-search.svg"
-import data from "./data.json"
+import React from "react";
+import "./mainApp.scss";
+import ContentBox from "./contentBox.js";
+import searchBox from "../../assets/icon-search.svg";
+import Trending from "./trending.js";
+import Recommend from "./recommend.js";
+import data from "./data.json";
 
-export default function mainApp() {
-  fetch()
-     .then(res=>{
-        this.setState({package:[...this.state.package,res.json()]}) 
-        //using es6
-     })
-     .catch(err=>{
-        console.log(err)
-     })
-
+export default function mainApp() {  
   return (
-    <div className='mainApp'>
+    <div className="mainApp">
       <ContentBox></ContentBox>
-      <div className='searchBox'>
+      <div className="searchBox">
         <img src={searchBox} alt="searchIcon"></img>
-        <input type="text" placeholder='Search for movies or TV series'></input>     
+        <input type="text" placeholder="Search for movies or TV series"></input>
       </div>
-      
-      <div className='trending'>
-        
-      </div>
+
+      <h2>Trending</h2>
+      <Trending usedData={data}></Trending>
+
+      <h2>Recommend for you</h2>      
+      <Recommend usedData={data}></Recommend>
     </div>
-  )
+  );
 }
---
+++ entertaiment-web-app/src/components/mainApp/recommend.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./recommend.scss";
+import movie from "../../assets/icon-nav-movies.svg";
+import tvSeries from "../../assets/icon-nav-tv-series.svg";
+import marked from "../../assets/icon-bookmark-full.svg";
+import unMarked from "../../assets/icon-bookmark-empty.svg";
+
+export default function recommend(props) {
+  return (
+    <div className="recommend">
+      {props.usedData.map((index) =>
+        index.isTrending === true ? (
+          ""
+        ) : (
+          <div className="recommendMovie">
+            <img
+              src={index.thumbnail.regular.small}
+              alt={"movie" + index.title}
+            ></img>
+
+            <div className="fullDesc">
+              <div className="description">
+                <p>{index.year}</p>
+                {index.category === "Movie" ? (
+                  <img src={movie} alt="movie"></img>
+                ) : (
+                  <img src={tvSeries} alt="tvSeries"></img>
+                )}
+                <p>{index.category}</p>
+                <p>{index.rating}</p>
+              </div>
+              <h3>{index.title}</h3>
+            </div>
+
+            {index.isBookmarked ? (
+              <div className="mark">
+                <img src={marked} alt="marked"></img>
+              </div>
+            ) : (
+              <div className="mark">
+                <img src={unMarked} alt="unMarked"></img>
+              </div>
+            )}
+          </div>
+        )
+      )}
+    </div>
+  );
+}
